923697c8bc0f10afc735138be2c8cc62e23a644d
Andi Rustianto <anditodev46@gmail.com>
2024-09-21 17:33:43 +0700

    textfield

---
+++ 00-intro-to-flutter/00-19-TextfieldWidget/textfieldwidget/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan text field"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextField(
+                controller: controller,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                obscureText: false,
+                maxLength: 5,
+                maxLines: 1,
+              ),
+              Text(controller.text)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
