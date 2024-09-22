c6310a4479b96221e6a702ecc2e27d08954e8a06
Andi Rustianto <anditodev46@gmail.com>
2024-09-22 15:43:50 +0700

    Custom Card

---
+++ 00-intro-to-flutter/00-23-opacity/opacity/lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Custom Card Example",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF8C062F),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xFFFE5788), Color(0xFFF56D56D)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 15,
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://marketplace.canva.com/EAF3DZwB4GQ/1/0/566w/canva-pink-and-cream-watercolor-background-document-%28a4%29-g_dJrvT_rUs.jpg"),
+                                fit: BoxFit.cover)),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12)),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://upload.wikimedia.org/wikipedia/id/thumb/b/b1/Merdeka_Square_Monas_02.jpg/800px-Merdeka_Square_Monas_02.jpg"),
+                              fit: BoxFit.fill)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          20,
+                          50 + MediaQuery.of(context).size.height * 0.35,
+                          20,
+                          20),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Monumen Nasional, Jakarta, Indonesia",
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color(0xFFF56D5D),
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Posted on ",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    "September, 18 2024",
+                                    style: TextStyle(
+                                      color: Color(0xFFF56D5D),
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Spacer(
+                                  flex: 10,
+                                ),
+                                // Icon
+                                Icon(
+                                  Icons.thumb_up,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                // Text
+                                Text(
+                                  "1.2K",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 5,
+                                ),
+                                // Icon
+                                Icon(
+                                  Icons.comment,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                // Text
+
+                                Text(
+                                  "76",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 10,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
