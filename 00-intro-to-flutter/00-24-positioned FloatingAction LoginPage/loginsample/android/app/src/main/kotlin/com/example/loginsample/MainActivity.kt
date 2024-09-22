a3db64551a41d4223244c8645566ada03522d873
Andi Rustianto <anditodev46@gmail.com>
2024-09-22 19:10:59 +0700

    login page

---
+++ 00-intro-to-flutter/00-24-positioned FloatingAction LoginPage/loginsample/android/app/src/main/kotlin/com/example/loginsample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loginsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 00-intro-to-flutter/00-24-positioned FloatingAction LoginPage/loginsample/lib/main.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: true,
+      // debugShowMaterialGrid: true,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getBigDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEEEEEE),
+      body: Stack(
+        children: [
+          Positioned(
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            left: -getSmallDiameter(context) / 4,
+            top: -getSmallDiameter(context) / 4,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            right: -getSmallDiameter(context) / 2,
+            bottom: -getSmallDiameter(context) / 2,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0xFFF3E9EE)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5)),
+                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: const Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.email,
+                              color: Color(0xFFFF4891),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFFFF4891))),
+                            labelText: "Email : ",
+                            labelStyle: TextStyle(color: Color(0xFFFF4891))),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.vpn_key,
+                              color: Color(0xFFFF4891),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFFFF4891))),
+                            labelText: "Password : ",
+                            labelStyle: TextStyle(color: Color(0xFFFF4891))),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 20, 20),
+                    child: const Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Color(0xFFFF4891), fontSize: 12),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 40,
+                        child: Container(
+                          child: Container(
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.transparent,
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(20),
+                                splashColor: Colors.blue.shade200,
+                                onTap: () {},
+                                child: Center(
+                                  child: Text(
+                                    "Sign In",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                                colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter),
+                          ),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        mini: true,
+                        elevation: 0,
+                        onPressed: null,
+                        child:
+                            Image(image: AssetImage("assets/icon/google.png")),
+                      ),
+                      FloatingActionButton(
+                        mini: true,
+                        elevation: 0,
+                        onPressed: null,
+                        child: Image(
+                            image: AssetImage("assets/icon/facebook.png")),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Dont't hava an account? ",
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Color(0xFFFF4891),
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
