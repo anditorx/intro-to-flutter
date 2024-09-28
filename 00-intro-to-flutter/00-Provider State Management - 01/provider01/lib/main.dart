90225a097b60c61d0a25323dd81cf9c637c344db
Andi Rustianto <anditodev46@gmail.com>
2024-09-28 17:17:54 +0700

    learn provider state management

---
+++ 00-intro-to-flutter/00-Provider State Management - 01/provider01/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider01/application_color.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ChangeNotifierProvider<ApplicationColor>(
+        create: (context) =>
+            ApplicationColor(), // Mengganti 'builder' dengan 'create'
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+              builder: (context, applicationColor, _) => Text(
+                "Provider State Management - 1",
+                style: TextStyle(color: applicationColor.color),
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<ApplicationColor>(
+                  builder: (context, applicationColor, _) => AnimatedContainer(
+                    duration: Duration(microseconds: 200),
+                    width: 100,
+                    height: 100,
+                    color: applicationColor.color,
+                    margin: EdgeInsets.all(5),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
+                    Consumer<ApplicationColor>(
+                      builder: (context, applicationColor, _) => Switch(
+                        value: applicationColor.isLightBlue,
+                        onChanged: (newValue) {
+                          applicationColor.isLightBlue = newValue;
+                        },
+                      ),
+                    ),
+                    Container(margin: EdgeInsets.all(5), child: Text("LB")),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
