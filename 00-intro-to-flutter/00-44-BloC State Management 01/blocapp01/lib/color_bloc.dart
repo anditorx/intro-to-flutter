dbfd9d901be463863d6527d0d54a35bbe79b6e74
Andi Rustianto <anditodev46@gmail.com>
2024-09-30 01:19:33 +0700

    learn state management - BloC State Management

--- 00-intro-to-flutter/00-44-BloC State Management 01/blocapp01/lib/color_bloc.dart
+++ 00-intro-to-flutter/00-44-BloC State Management 01/blocapp01/lib/color_bloc.dart
@@ -8,9 +8,9 @@ class ColorBloc {
 
   final StreamController<ColorEvent> _eventController =
       StreamController<ColorEvent>();
-  final StreamController<Color> _stateController = StreamController<Color>();
-
   StreamSink<ColorEvent> get eventSink => _eventController.sink;
+
+  final StreamController<Color> _stateController = StreamController<Color>();
   StreamSink<Color> get _stateSink => _stateController.sink;
 
   Stream<Color> get stateStream => _stateController.stream;
